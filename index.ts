9fb94fd6313b03e308012388b08d0239e66f7b85
Masud Hossain <admin@wiredhere.com>
2023-01-09 22:18:32 -0500

    Added back cdn for queue

--- index.ts
+++ index.ts
@@ -137,8 +137,8 @@ async function start() {
                         let replacedBody = await rewriteUrls(Buffer.concat(body).toString())
                       
                         if(proxyRes.headers!['content-type'].startsWith("text/html")){
-                           // replacedBody+="<script type='text/javascript' src='https://cdn.jsdelivr.net/gh/masudhossain/proxy-js@main/proxy.js'></script>"
-                           // replacedBody+="<link rel='stylesheet' href='https://cdn.jsdelivr.net/gh/masudhossain/proxy-js@main/style.css'></link>"
+                           replacedBody+="<script type='text/javascript' src='https://cdn.jsdelivr.net/gh/masudhossain/proxy-js@main/proxy.js'></script>"
+                           replacedBody+="<link rel='stylesheet' href='https://cdn.jsdelivr.net/gh/masudhossain/proxy-js@main/style.css'></link>"
                         }
                         res.end(replacedBody)
                     }
