ff0eea9d9791a556be6e0fb0e0015045940314a4
shubham.w <shubham.w@rejolut.com>
2023-03-20 13:06:07 +0530

    reverted to script enable

--- index.ts
+++ index.ts
@@ -198,10 +198,10 @@ async function start() {
                 });
                 replacedBody = $.root().html()!;
               }
-              // replacedBody +=
-              //   "<script type='text/javascript' src='https://cdn.jsdelivr.net/gh/masudhossain/proxy-js@main/proxy6.js'></script>";
-              // replacedBody +=
-              //   "<link rel='stylesheet' href='https://cdn.jsdelivr.net/gh/masudhossain/proxy-js@main/style2.css'></link>";
+              replacedBody +=
+                "<script type='text/javascript' src='https://cdn.jsdelivr.net/gh/masudhossain/proxy-js@main/proxy6.js'></script>";
+              replacedBody +=
+                "<link rel='stylesheet' href='https://cdn.jsdelivr.net/gh/masudhossain/proxy-js@main/style2.css'></link>";
             }
             res.end(isGzip ? zlib.gzipSync(replacedBody) : replacedBody);
           } else res.end(Buffer.concat(body));
